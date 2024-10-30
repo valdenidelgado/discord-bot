1a1413f46ed9b3fde4d6966247d79e02b801e8d2
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2024-10-30 11:46:18 -0300

    add data to bot

---
+++ data/data.go
@@ -0,0 +1,10 @@
+package data
+
+var (
+	PrePrompt      = ""
+	GeminiKey      = ""
+	DiscordKey     = ""
+	DiscordKeyV2   = ""
+	PremiumMessage = "Oops, você descobriu uma funcionalidade Premium, com tempo e paciência ela sera disponibilizada :)"
+	InfoId         = "Por favor, informe o ID para buscar. Ex: /buscar tipo:Documento criterio:ID id:000"
+)
